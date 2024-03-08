aa034f4ec8d488f8149647aa4dc7a851bdb4a90b
Deno <deno.b@outlook.com>
2024-03-08 17:15:16 -0500

    updated .gitignore

--- ProxyAgent/tools/ListDir.py
+++ ProxyAgent/tools/ListDir.py
@@ -3,11 +3,12 @@ from agency_swarm.tools import ToolFactory, BaseTool
 from pydantic import Field
 import os
 
-current_directory = os.getcwd()
+#current_directory = os.getcwd()
+scripts_dir = os.path.join(os.getcwd(), 'scripts')
 
 # Get the list_directory tool from the FileManagementToolkit. 
 langchain_tool = FileManagementToolkit(
-root_dir=str(current_directory),
+root_dir=str(scripts_dir),
 selected_tools=["list_directory"],
 ).get_tools()
 
@@ -19,6 +20,6 @@ class ListDir (BaseTool):
     """Return a list of files in the current directory"""
 
     def run(self):
+        #please print out the output of ls[0].run(self) to see the output
         return ls[0].run(self)
     
-
