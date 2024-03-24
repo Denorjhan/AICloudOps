e539c2c665c6f48e275974f7f48651f286baf799
Deno <deno.b@outlook.com>
2024-03-24 16:09:56 -0400

    updated .env.example

--- aicloudops/app/modules/docker_exec.py
+++ aicloudops/app/modules/docker_exec.py
@@ -95,7 +95,8 @@ class ContainerPathDockerExecutor(DockerCommandLineCodeExecutor):
         container_work_dir = Path(container_work_dir).absolute()
         creds = {
             "AWS_ACCESS_KEY_ID": os.getenv("AWS_ACCESS_KEY_ID"),
-            "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_SECRET_ACCESS_KEY")
+            "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_SECRET_ACCESS_KEY"),
+            "AWS_DEFAULT_REGION": os.getenv("AWS_DEFAULT_REGION")
         }
         
         self._container = client.containers.create(
